dcf416c15c90f2fed7718fcc89d6d4cc42a613f0
chaseBAT <21925076@zju.edu.cn>
2020-07-06 22:42:49 +0800

    first提交

---
+++ src/main/java/com/itheima/dao/UserDao.java
@@ -0,0 +1,50 @@
+package com.itheima.dao;
+
+import com.itheima.domain.User;
+
+import java.util.List;
+/**
+ * 用户的持久层接口
+ */
+public interface UserDao {
+
+    /**
+     * 查询所有用户
+     * @return
+     */
+    List<User> findAll();
+
+    /**
+     * 保存用户
+     * @param user
+     */
+    void saveUser(User user);
+
+    /**
+     * 更新用户
+     */
+    void updateUser(User user);
+
+    /**
+     * 删除用户
+     */
+    void deleteUser(Integer userId);
+
+    /**
+     * 根据id查询用户信息
+     */
+    User findById(Integer userId);
+
+    /**
+     * 根据名称模糊查询用户信息
+     * @param username
+     * @return
+     */
+    List<User> findByName(String username);
+
+    /**
+     * 查询总用户数
+     * @return
+     */
+    int findTotal();
+}
